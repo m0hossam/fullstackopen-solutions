f28e6f4ed7cec5c26364e33d61402d59f998da63
m0hossam <mohossam1326@gmail.com>
2024-08-11 23:03:01 +0300

    Change url to relative path & add vite config proxy for development

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/persons';
+const baseUrl = '/api/persons';
 
 // returns persons array as a promise
 const getAllPersons = () => axios.get(baseUrl).then(reponse => reponse.data);
